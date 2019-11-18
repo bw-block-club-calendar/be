d72f7c171944739007246f45af3afd4d5103228f
gelinas <jlgelinas@gmail.com>
2019-11-18 18:05:37 -0500

    fix users migration

--- data/testing/migrations/20191117182057_users_table.js
+++ data/testing/migrations/20191117182057_users_table.js
@@ -16,24 +16,6 @@ exports.up = function(knex) {
 
     users.string('type', 32)
       .notNullable();
-
-    // define a Foreign Key
-    users.integer('profile_id') // foreign key to profile table
-        .unsigned() // integer without negative values, db uses sign bit for larger #s
-        .references('id')
-        .inTable('profiles')
-        .onDelete('SET NULL') // regards deleting recrod from the primary key table
-        // onDelete() can take 'RESTRICT', 'NO ACTION', 'SET NULL', 'CASCADE'
-        .onUpdate('CASCADE'); // regards chaging the value of the primary key table
-
-    // define a Foreign Key
-    users.integer('organization_id') // foreign key to profile table
-        .unsigned() // integer without negative values, db uses sign bit for larger #s
-        .references('id')
-        .inTable('organizations')
-        .onDelete('SET NULL') // regards deleting recrod from the primary key table
-        // onDelete() can take 'RESTRICT', 'NO ACTION', 'SET NULL', 'CASCADE'
-        .onUpdate('CASCADE'); // regards chaging the value of the primary key table
   });
 };
 
