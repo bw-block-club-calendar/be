2bc0c715cc618e6edfb67ff6ffc491722c868a9b
gelinas <jlgelinas@gmail.com>
2019-11-18 18:01:29 -0500

    migrations typo

--- knexfile.js
+++ knexfile.js
@@ -48,7 +48,7 @@ module.exports = {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './data/migations'
+      directory: './data/migrations'
     },
     seeds: {
       directory: './data/seeds'
