1e1ac9f5231f4e5e954485f6867400dc3eb7a62f
gelinas <jlgelinas@gmail.com>
2019-11-21 13:38:43 -0500

    knex cleaner comments

--- data/seeds/000_cleaner.js
+++ data/seeds/000_cleaner.js
@@ -1,6 +1,6 @@
-const cleaner = require('knex-cleaner');
+// const cleaner = require('knex-cleaner');
 
 exports.seed = function(knex, Promise) {
   // return cleaner.clean(knex); // cleans all tables and resets primary keys
-  // is clearing my knex tables and keeping me from rolling back
+  // must find way to ignore knex_migrations and knex_migrations_lock
 }
--- data/testing/seeds/000_cleaner.js
+++ data/testing/seeds/000_cleaner.js
@@ -2,4 +2,5 @@
 
 exports.seed = function(knex, Promise) {
   // return cleaner.clean(knex); // cleans all tables and resets primary keys
+  // must find way to ignore knex_migrations and knex_migrations_lock
 }
